70ba3bafaa791a34f88cdaed93c87bc6c27f3069
Tal Klein <161637090+TalKleinBgu@users.noreply.github.com>
2026-03-27 16:03:18 +0300

    update sidebars

--- pages/5_Minesweeper.py
+++ pages/5_Minesweeper.py
@@ -4,7 +4,7 @@ import sys
 
 import streamlit as st
 
-st.set_page_config(page_title="SQL Minesweeper - HW1", page_icon="💣", layout="wide")
+st.set_page_config(page_title="SQL Minesweeper - HW1", page_icon="💣", layout="wide", initial_sidebar_state="collapsed")
 
 st.markdown(
     """
